0d1b6311297830461006f987c46134b063e75601
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-11-23 21:24:56 +0530

    Event binding in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,21 @@ export class AppComponent {
   title: string;
   logo: string = 'img/logo.svg';
   name: string = "Manoj";
+  
   constructor() {
     this.title = "Ultimate Angular";
+  }
+
+  handleBlur(event: any) {
+    this.name = event.target.value;
+    console.log(event);
+  }
+
+  handleInput(event: any) {
+    this.name = event.target.value;
+  }
+
+  handleClick() {
+    this.name = 'Patra';
   }
 }
