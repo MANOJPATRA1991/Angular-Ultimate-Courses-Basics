70481cbfc6f4e5e1539e085d0e00edb2d92a05e5
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-11-23 21:02:00 +0530

    Bootstrapping an angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "my-app",
+  selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "Angular";
+  title: string;
+  constructor() {
+    this.title = "Ultimate Angular";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
+// Needed if we are implementing a browser app
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ BrowserModule, CommonModule ],
+  // Components that belong to this module
+  // Declarables: components, directives and pipes
+  // Declarables must belong to exactly one module
+  declarations: [ AppComponent ],
+  // Only the root module contains the bootstrap component
+  // AppComponent is the root component that Angular creates and inserts into the index.html host web page.
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
