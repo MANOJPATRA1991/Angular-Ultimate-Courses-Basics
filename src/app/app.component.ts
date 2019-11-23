b571aedaad7ed2848654c7e302018010dc1b7a5f
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-11-23 21:55:39 +0530

    ngFor and iterating collections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,36 @@
 import { Component } from "@angular/core";
 
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+};
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title: string;
-  logo: string = 'img/logo.svg';
-  name: string = "Manoj";
-  
-  constructor() {
-    this.title = "Ultimate Angular";
-  }
-
-  handleChange(value: string) {
-    this.name = value;
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }];
 }
