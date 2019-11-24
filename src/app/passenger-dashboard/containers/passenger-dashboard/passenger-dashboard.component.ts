baf0d4570e6ecdca39c51cde0faa57fce9bed5d4
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-11-24 11:29:46 +0530

    Add presentational (dumb) components

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -7,39 +7,45 @@ import  { Passenger } from '../../models/passenger.interface';
   styleUrls: ['./passenger-dashboard.component.css']
 })
 export class PassengerDashboardComponent implements OnInit {
-  passengers: Passenger[] = [{
-    id: 1,
-    fullname: 'Stephen',
-    checkedIn: true,
-    checkInDate: 1490742000000,
-    children: null
-  }, {
-    id: 2,
-    fullname: 'Rose',
-    checkedIn: false,
-    checkInDate: null,
-    children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
-  }, {
-    id: 3,
-    fullname: 'James',
-    checkedIn: true,
-    checkInDate: 1491606000000,
-    children: null
-  }, {
-    id: 4,
-    fullname: 'Louise',
-    checkedIn: true,
-    checkInDate: 1488412800000,
-    children: [{ name: 'Jessica', age: 1 }]
-  }, {
-    id: 5,
-    fullname: 'Tina',
-    checkedIn: false,
-    checkInDate: null,
-    children: null
-  }];
+  passengers: Passenger[]
 
+  // Used for services
+  constructor() { }
+
+  // Data fetching should be done here
   ngOnInit() {
+    console.log('ngOnInit');
+    this.passengers = [{
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true,
+      checkInDate: 1490742000000,
+      children: null
+    }, {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false,
+      checkInDate: null,
+      children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
+    }, {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true,
+      checkInDate: 1491606000000,
+      children: null
+    }, {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true,
+      checkInDate: 1488412800000,
+      children: [{ name: 'Jessica', age: 1 }]
+    }, {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false,
+      checkInDate: null,
+      children: null
+    }];
   }
 
 }
