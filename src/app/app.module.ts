62ebfdf57de6667eb7a809e493dfc66f99b612c7
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-02 03:26:17 +0530

    root module routes and outlets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './database/in-memory-data.service';
@@ -11,13 +11,20 @@ import { InMemoryDataService } from './database/in-memory-data.service';
 import { AppComponent } from './app.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+
+];
+
 @NgModule({
   imports: [ 
     // Angular modules
     BrowserModule, 
     CommonModule,
     HttpClientModule,
-    RouterModule,
+    RouterModule.forRoot(routes),
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     ),
@@ -27,7 +34,10 @@ import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashbo
   // Components that belong to this module
   // Declarables: components, directives and pipes
   // Declarables must belong to exactly one module
-  declarations: [ AppComponent ],
+  declarations: [ 
+    AppComponent, 
+    HomeComponent 
+  ],
   // Only the root module contains the bootstrap component
   // AppComponent is the root component that Angular creates and inserts into the index.html host web page.
   bootstrap: [ AppComponent ],
