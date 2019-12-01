d7ae14b97fda96be778058434ea8fde62f810324
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-02 03:44:37 +0530

    Dynamic navigation with *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
 import { Component } from "@angular/core";
 
+interface Nav {
+  link: string,
+  name: string,
+  exact?: boolean
+}
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  nav: Nav[] = [
+    {
+      link: '/',
+      name: 'Home',
+      exact: true
+    },
+    {
+      link: '/oops',
+      name: '404',
+    }
+  ];
+}
