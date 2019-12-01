c93d5d167d0aac52bbc5c863dbca73cd9e04f276
MANOJPATRA1991 <patra.manoj0@gmail.com>
2019-12-02 04:32:15 +0530

    Applying redirects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'passengers', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 
 ];
